706b1064ba13a67f786cd9f6c34e48de75a9ad20
satoshi256kbyte <satoshi.kaneyasu@serverworks.co.jp>
2026-03-21 13:37:21 +0900

    test: 5.2 VerifyFunctionConstruct のユニットテスト

---
+++ test/constructs/verify-function.test.ts
@@ -0,0 +1,96 @@
+import * as cdk from "aws-cdk-lib";
+import { Template, Match } from "aws-cdk-lib/assertions";
+import { NetworkConstruct } from "../../lib/constructs/network";
+import { AuroraConstruct } from "../../lib/constructs/aurora";
+import { VerifyFunctionConstruct } from "../../lib/constructs/verify-function";
+
+describe("VerifyFunctionConstruct", () => {
+  let template: Template;
+
+  beforeAll(() => {
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, "TestStack");
+    const network = new NetworkConstruct(stack, "Network");
+    const aurora = new AuroraConstruct(stack, "Aurora", {
+      vpc: network.vpc,
+      auroraSg: network.auroraSg,
+    });
+    new VerifyFunctionConstruct(stack, "VerifyFunction", {
+      vpc: network.vpc,
+      lambdaSg: network.lambdaSg,
+      auroraCluster: aurora.cluster,
+      auroraSecret: aurora.secret,
+      opensearchCollectionEndpoint:
+        "https://dummy-endpoint.aoss.amazonaws.com",
+    });
+    template = Template.fromStack(stack);
+  });
+
+  test("Lambda 関数が Python 3.13 ランタイムで作成される", () => {
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      Runtime: "python3.13",
+    });
+  });
+
+  test("Lambda 関数のメモリが 256 MB である", () => {
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      MemorySize: 256,
+    });
+  });
+
+  test("Lambda 関数のタイムアウトが 300 秒である", () => {
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      Timeout: 300,
+    });
+  });
+
+  test("Lambda 関数が VPC 内に配置される", () => {
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      VpcConfig: Match.objectLike({
+        SubnetIds: Match.anyValue(),
+        SecurityGroupIds: Match.anyValue(),
+      }),
+    });
+  });
+
+  test("Lambda 関数に正しい環境変数が設定される", () => {
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      Environment: {
+        Variables: Match.objectLike({
+          AURORA_SECRET_ARN: Match.anyValue(),
+          AURORA_CLUSTER_ENDPOINT: Match.anyValue(),
+          OPENSEARCH_ENDPOINT:
+            "https://dummy-endpoint.aoss.amazonaws.com",
+          POWERTOOLS_SERVICE_NAME: "vector-verify",
+          POWERTOOLS_LOG_LEVEL: "INFO",
+        }),
+      },
+    });
+  });
+
+  test("IAM ポリシーに aoss:APIAccessAll が含まれる", () => {
+    template.hasResourceProperties("AWS::IAM::Policy", {
+      PolicyDocument: Match.objectLike({
+        Statement: Match.arrayWith([
+          Match.objectLike({
+            Action: "aoss:APIAccessAll",
+            Effect: "Allow",
+          }),
+        ]),
+      }),
+    });
+  });
+
+  test("IAM ポリシーに secretsmanager:GetSecretValue が含まれる", () => {
+    template.hasResourceProperties("AWS::IAM::Policy", {
+      PolicyDocument: Match.objectLike({
+        Statement: Match.arrayWith([
+          Match.objectLike({
+            Action: Match.arrayWith(["secretsmanager:GetSecretValue"]),
+            Effect: "Allow",
+          }),
+        ]),
+      }),
+    });
+  });
+});
